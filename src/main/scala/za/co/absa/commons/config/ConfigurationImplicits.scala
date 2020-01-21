fa94391153f9a890777b7b556087ceec362dd919
wajda <oleksandr.vayda@gmail.com>
2020-01-21 21:56:46 +0100

    spline #5 Nit fixes

--- src/main/scala/za/co/absa/commons/config/ConfigurationImplicits.scala
+++ src/main/scala/za/co/absa/commons/config/ConfigurationImplicits.scala
@@ -39,70 +39,70 @@ object ConfigurationImplicits {
       *
       * @return A value of string configuration property if exists, otherwise throws an exception.
       */
-    def getRequiredString: (String) => String = getRequired(conf.getString, isNotBlank)
+    def getRequiredString: String => String = getRequired(conf.getString, isNotBlank)
 
     /**
       * Gets a value of string array configuration property and checks whether the array is not empty.
       *
       * @return A value of string array configuration property if not empty, otherwise throws an exception.
       */
-    def getRequiredStringArray: (String) => Array[String] = getRequired(conf.getStringArray, (i: Array[String]) => i.nonEmpty)
+    def getRequiredStringArray: String => Array[String] = getRequired(conf.getStringArray, (i: Array[String]) => i.nonEmpty)
 
     /**
       * Gets a value of boolean configuration property and checks whether property exists.
       *
       * @return A value of boolean configuration property if exists, otherwise throws an exception.
       */
-    def getRequiredBoolean: (String) => Boolean = getRequired(conf.getBoolean(_, null), null.!=) //NOSONAR
+    def getRequiredBoolean: String => Boolean = getRequired(conf.getBoolean(_, null), null.!=) //NOSONAR
 
     /**
       * Gets a value of big decimal configuration property and checks whether property exists.
       *
       * @return A value of big decimal configuration property if exists, otherwise throws an exception.
       */
-    def getRequiredBigDecimal: (String) => BigDecimal = getRequired(conf.getBigDecimal, null.!=) //NOSONAR
+    def getRequiredBigDecimal: String => BigDecimal = getRequired(conf.getBigDecimal, null.!=) //NOSONAR
 
     /**
       * Gets a value of byte configuration property and checks whether property exists.
       *
       * @return A value of byte configuration property if exists, otherwise throws an exception.
       */
-    def getRequiredByte: (String) => Byte = getRequired(conf.getByte(_, null), null.!=) //NOSONAR
+    def getRequiredByte: String => Byte = getRequired(conf.getByte(_, null), null.!=) //NOSONAR
 
     /**
       * Gets a value of short configuration property and checks whether property exists.
       *
       * @return A value of short configuration property if exists, otherwise throws an exception.
       */
-    def getRequiredShort: (String) => Short = getRequired(conf.getShort(_, null), null.!=) //NOSONAR
+    def getRequiredShort: String => Short = getRequired(conf.getShort(_, null), null.!=) //NOSONAR
 
     /**
       * Gets a value of int configuration property and checks whether property exists.
       *
       * @return A value of int configuration property if exists, otherwise throws an exception.
       */
-    def getRequiredInt: (String) => Int = getRequired(conf.getInteger(_, null), null.!=) //NOSONAR
+    def getRequiredInt: String => Int = getRequired(conf.getInteger(_, null), null.!=) //NOSONAR
 
     /**
       * Gets a value of long configuration property and checks whether property exists.
       *
       * @return A value of long configuration property if exists, otherwise throws an exception.
       */
-    def getRequiredLong: (String) => Long = getRequired(conf.getLong(_, null), null.!=) //NOSONAR
+    def getRequiredLong: String => Long = getRequired(conf.getLong(_, null), null.!=) //NOSONAR
 
     /**
       * Gets a value of float configuration property and checks whether property exists.
       *
       * @return A value of float configuration property if exists, otherwise throws an exception.
       */
-    def getRequiredFloat: (String) => Float = getRequired(conf.getFloat(_, null), null.!=) //NOSONAR
+    def getRequiredFloat: String => Float = getRequired(conf.getFloat(_, null), null.!=) //NOSONAR
 
     /**
       * Gets a value of double configuration property and checks whether property exists.
       *
       * @return A value of double configuration property if exists, otherwise throws an exception.
       */
-    def getRequiredDouble: (String) => Double = getRequired(conf.getDouble(_, null), null.!=) //NOSONAR
+    def getRequiredDouble: String => Double = getRequired(conf.getDouble(_, null), null.!=) //NOSONAR
 
     private def getRequired[V](get: String => V, check: V => Boolean)(key: String): V =
       Try(get(key))
