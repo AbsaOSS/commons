f2f36eeb65f63727a04d5ce5a39163e7e535fae3
Bartlomiej Baj <bajbartlomiej997@gmail.com>
2022-09-13 12:43:37 +0200

    #107 Deprecate OptionImplicits and CollectionImplicits

--- src/main/scala/za/co/absa/commons/lang/OptionImplicits.scala
+++ src/main/scala/za/co/absa/commons/lang/OptionImplicits.scala
@@ -18,6 +18,7 @@ package za.co.absa.commons.lang
 
 import za.co.absa.commons.lang.TypeConstraints.not
 
+@deprecated("Use type-specific ...Extension instead")
 object OptionImplicits {
 
   implicit class StringWrapper(val s: String) extends AnyVal {
